7fdbb74c9e0e3b2eb0b981968be7611c173f05ab
Varun Sharma <sharma-varun@users.noreply.github.com>
2016-09-23 17:38:56 -0500

    3rd

--- Todo/app.js
+++ Todo/app.js
@@ -4,4 +4,28 @@ var data = [
   date: new Date(),
   status: false
   }
-]
+];
+
+
+//We will read the data in the controller
+// then pass it ro the view
+
+var controller = {
+  init: function(){
+  },
+  addTask: function(){
+
+  },
+  removeTask: function(){
+
+  },
+  //Other tasks accordingly 
+
+};
+
+
+var view = {
+
+  init: function(){},
+  render: function(){}
+};
